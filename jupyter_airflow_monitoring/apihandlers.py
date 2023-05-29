ca3349f8efd1f8ef2a300185853aa6acac769c4e
Emile Duquennoy <emile.duquennoy@student.junia.com>
2023-05-29 10:37:54 +0200

    NEW:: Secure auth for /message API endpoints

--- jupyter_airflow_monitoring/apihandlers.py
+++ jupyter_airflow_monitoring/apihandlers.py
@@ -1,4 +1,5 @@
-from notebook.base.handlers import IPythonHandler
+from notebook.base.handlers import APIHandler
+from tornado import web
 import json
 import os
 
@@ -13,10 +14,13 @@ def set_message(message, title, color):
     with open("/tmp/cache_extension.txt", "w") as f:
         f.write(json.dumps({"message": message, "title": title, "color": color}))
 
-class MessageHandler(IPythonHandler):
+class MessageHandler(APIHandler):
+
+    @web.authenticated
     def get(self):
         self.finish(get_message())
 
+    @web.authenticated
     def post(self):
         message = self.get_body_argument('message', '')
         title = self.get_body_argument('title', '')
